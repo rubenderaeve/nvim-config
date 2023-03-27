b2ce3a0ecee6301c665b67aea78c12a32f4ad631
Ruben De Raeve <rderaeve@gmail.com>
2023-03-27 21:56:10 +0200

    update nvim_setup_windows.ps1
    - lua-language-server

--- docs/nvim_setup_windows.ps1
+++ docs/nvim_setup_windows.ps1
@@ -36,19 +36,7 @@ scoop install vcredist2022
 scoop install 7zip
 
 # Install lua-language-server
-$lua_ls_link = "https://github.com/LuaLS/lua-language-server/releases/download/3.6.11/lua-language-server-3.6.11-win32-x64.zip"
-$lua_ls_install_dir = "D:\portable_tools"
-$lua_ls_src_path = "$lua_ls_install_dir\lua-language-server.zip"
-$lua_ls_dir = "$lua_ls_install_dir\lua-language-server"
-
-# Download file, ref: https://stackoverflow.com/a/51225744/6064933
-Invoke-WebRequest $lua_ls_link -OutFile "$lua_ls_src_path"
-
-# Extract the zip file using 7zip, ref: https://stackoverflow.com/a/41933215/6064933
-7z x "$lua_ls_src_path" -o"$lua_ls_dir"
-
-# Setup PATH env variable, ref: https://stackoverflow.com/q/714877/6064933
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$lua_ls_dir\bin", "Machine")
+scoop install lua-language-server
 
 # Install neovim nightly
 scoop bucket add versions
