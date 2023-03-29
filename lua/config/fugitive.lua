1dbeff5c31824cb7ed534fcd449b0805ebbedab8
Ruben De Raeve <rderaeve@gmail.com>
2023-03-29 10:25:28 +0200

    fugitive.vim mappings
    - git commit -ev
    - Gread

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -6,6 +6,7 @@ keymap.set("n", "<leader>gc", "<cmd>Git commit -ev<cr>", { desc = "Git commit" }
 keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<cr>", { desc = "Git diff" })
 keymap.set("n", "<leader>gpl", "<cmd>Git pull<cr>", { desc = "Git pull" })
 keymap.set("n", "<leader>gpu", "<cmd>15 split|term git push<cr>", { desc = "Git push" })
+keymap.set("n", "<leader>gx", "<cmd>Gread<cr>", { desc = "Git checkout --filename" })
 
 -- convert git to Git in command line mode
 vim.fn['utils#Cabbrev']('git', 'Git')
